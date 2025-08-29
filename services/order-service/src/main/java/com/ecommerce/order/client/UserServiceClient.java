bd4fc8dfbef10f6dc98f12d7fcca2691529bafe1
khghouse <khghouse@naver.com>
2025-08-30 00:34:03 +0900

    서비스 모듈명 변경 (service-user -> user-service)

--- services/order-service/src/main/java/com/ecommerce/order/client/UserServiceClient.java
+++ services/order-service/src/main/java/com/ecommerce/order/client/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-user")
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
 
     @GetMapping("/api/users/{id}")
